0201375e7432fdff16d3e478a8149eabe97e51cb
kk9923 <13047381556@163.com>
2018-08-14 17:32:33 +0800

    提交简单工厂模式下的代码

--- app/src/main/java/com/kx/designpatterns/MainActivity.java
+++ app/src/main/java/com/kx/designpatterns/MainActivity.java
@@ -1,13 +1,34 @@
 package com.kx.designpatterns;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.kx.designpatterns.SimpleFactoryPattern.SimpleFactoryActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.simpleFactoryActivity).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.simpleFactoryActivity :
+                startActivity(SimpleFactoryActivity.class);
+                break;
+        }
+    }
+
+
+    private void startActivity(Class activityClass){
+        Intent intent = new Intent(this,activityClass);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/kx/designpatterns/SimpleFactoryPattern/ManToy.java
@@ -0,0 +1,16 @@
+package com.kx.designpatterns.SimpleFactoryPattern;
+
+/**
+ * 具体的玩具类--- 女人玩具
+ */
+public class ManToy implements IToy {
+    @Override
+    public String getName() {
+        return "ManToy";
+    }
+
+    @Override
+    public float price() {
+        return 100;
+    }
+}
