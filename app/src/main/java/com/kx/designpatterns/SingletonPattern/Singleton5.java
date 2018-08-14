815adedb8c671f802b0962ad262a0d82a1bdb489
kk9923 <13047381556@163.com>
2018-08-14 21:49:40 +0800

    新增容器类的单例模式

--- app/src/main/java/com/kx/designpatterns/SingletonPattern/Singleton5.java
+++ app/src/main/java/com/kx/designpatterns/SingletonPattern/Singleton5.java
@@ -2,7 +2,7 @@ package com.kx.designpatterns.SingletonPattern;
 
 /**
  * 枚举单例模式
- * 线程安全，至此序列化与反序列化，不会创建多个对象
+ * 线程安全，支持序列化与反序列化，不会创建多个对象
  */
 public enum  Singleton5 {
     INSTANCE;
---
+++ app/src/main/java/com/kx/designpatterns/SingletonPattern/SingletonManager.java
@@ -0,0 +1,20 @@
+package com.kx.designpatterns.SingletonPattern;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by admin on 2018/8/13.
+ * 使用容器类实现的单例模式
+ */
+public class SingletonManager {
+    private static  Map<String,Object> mObjectMap = new HashMap<>();
+    public static void  registerService (String key,Object instance){
+        if (!mObjectMap.containsKey(key)){
+            mObjectMap.put(key,instance);
+        }
+    }
+    public static Object getService(String key){
+        return mObjectMap.get(key);
+    }
+}
